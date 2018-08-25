4f378297c02ba3d29526e29fc410a4848823d936
leofalves <leo.fabiano@gmail.com>
2018-08-25 13:50:15 -0300

    Initial commit

---
+++ Controllers/CarrinhoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Loja.DAO;
+using Loja.Models;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        //public string Get(int id)
+        public Carrinho Get(int id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(id);
+            //return carrinho.ToXml();
+            return carrinho;
+        }
+
+        public string Post([FromBody] Carrinho carrinho)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            dao.Adiciona(carrinho);
+            return "sucess";
+        }
+    }
+}
---
+++ DAO/CarrinhoDAO.cs
@@ -0,0 +1,51 @@
+﻿using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.DAO
+{
+    public class CarrinhoDAO
+    {
+        private static Dictionary<long, Carrinho> banco = new Dictionary<long, Carrinho>();
+        private static long contador = 1;
+
+        static CarrinhoDAO()
+        {
+            Produto videogame = new Produto(6237, "Videogame 4", 4000, 1);
+            Produto esporte = new Produto(3467, "Jogo de esporte", 60, 2);
+            Produto hotel = new Produto(444, "Hotel em Paris",2580, 5);
+            Carrinho carrinho = new Carrinho();
+            carrinho.Adiciona(videogame);
+            carrinho.Adiciona(esporte);
+            carrinho.Endereco = "Rua Vergueiro 3185, 8 andar, Sao Paulo";
+            carrinho.Id = 1;
+            banco.Add(1, carrinho);
+
+            //Carrinho carrinho2 = new Carrinho();
+            //carrinho2.Adiciona(hotel);
+            //carrinho2.Endereco = "Rua Corifeu de Azevedo Marques 874, Guarulhos";
+            //carrinho2.Id = 2;
+            //banco.Add(2, carrinho2);
+        }
+
+        public void Adiciona(Carrinho carrinho)
+        {
+            contador++;
+            carrinho.Id = contador;
+            banco.Add(contador, carrinho);
+        }
+
+        public Carrinho Busca(long id)
+        {
+            return banco[id];
+        }
+
+        public void Remove(long id)
+        {
+            banco.Remove(id);
+        }
+    }
+}
